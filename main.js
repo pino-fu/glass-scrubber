d22194cd867db01f0fa0aa94f13506d854d5fe04
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 10:44:44 -0600

    JS challenge involving accessing specificially indexed items within an array.

---
+++ main.js
@@ -0,0 +1,22 @@
+const dishes = [
+    "Dinner plate", "Water glass", "Salad bowl",
+    "Dinner plate", "Wine glass", "Spoon",
+    "Spoon", "Fork", "Salad bowl", "Whiskey glass",
+    "Fork", "Spoon", "Fork"
+]
+
+/*
+    Declare three variables to store the string value
+    of a glass in the array. Use the correct index to
+    get the right value.
+*/
+const dirty = dishes[1];
+const dirtier = dishes[4];
+const dirtiest = dishes[9];
+
+console.log("I am cleaning the following glasses:")
+
+// Put each variable in one of the parenthesis below
+console.log(dirty);
+console.log(dirtier);
+console.log(dirtiest);
